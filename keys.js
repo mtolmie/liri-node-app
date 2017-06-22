8eefce4c2d81432496ae4e192c7c366531462074
Michael <michaeltolmie@gmail.com>
2017-06-22 14:26:38 -0700

    liri-node-app

--- keys.js
+++ keys.js
@@ -1,12 +1,34 @@
-HEAD
-console.log('this is loaded');
 
-exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
-}
+
+
+
+
+
+// HEAD
+// console.log('this is loaded');
+
+// exports.twitterKeys = {
+//   consumer_key: '<input here>',
+//   consumer_secret: '<input here>',
+//   access_token_key: '<input here>',
+//   access_token_secret: '<input here>',
+// }
+
+
+
+// my-tweets
+// spotify-this-song
+// movie-this
+// do-what-it-says
+
+
+
+// exports.twitterKeys = {
+//   consumer_key: '<input here>',
+//   consumer_secret: '<input here>',
+//   access_token_key: '<input here>',
+//   access_token_secret: '<input here>',
+// }
 
 
 
@@ -16,25 +38,9 @@ movie-this
 do-what-it-says
 
 
-
-exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
-}
-
-
-
-my-tweets
-spotify-this-song
-movie-this
-do-what-it-says
-
-
-Spotify.....
-Client ID
-8fd0e0a552164a97a8730dc3c194894f
-Client Secret
-ad4bfaa77176dd2ec2e5a5f9f0ec5049e0b9f6f4
-6b05cdf24084463abede4401147c7dcd
+// Spotify.....
+// Client ID
+// 8fd0e0a552164a97a8730dc3c194894f
+// Client Secret
+// ad4bfaa77176dd2ec2e5a5f9f0ec5049e0b9f6f4
+// 6b05cdf24084463abede4401147c7dcd
