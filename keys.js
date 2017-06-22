ad4bfaa77176dd2ec2e5a5f9f0ec5049e0b9f6f4
Michael Tolmie <michaeltolmie@gmail.com>
2017-06-22 13:50:48 -0700

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,22 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
+
+
+
+my-tweets
+spotify-this-song
+movie-this
+do-what-it-says
+
+
+Spotify.....
+Client ID
+8fd0e0a552164a97a8730dc3c194894f
+Client Secret
+6b05cdf24084463abede4401147c7dcd
